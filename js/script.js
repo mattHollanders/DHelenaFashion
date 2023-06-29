ab3e17383a4aed70bf7f812b7a8275b899abef44
Mateus Hollanders <mateushollandersbraga@gmail.com>
2023-06-29 14:51:11 -0300

    arrumando carrossel e mobile

--- js/script.js
+++ js/script.js
@@ -1,29 +1,15 @@
 $(document).ready(function() {
-  // Manipular o evento de clique dos nav-items
-  $('.nav-item').click(function() {
-    // Remover todos os "active" dos nav-items
-    $('.nav-item').removeClass('active');
-    
-    // Obter o índice do nav-item clicado
-    var index = $(this).index();
-
-    // Adiciona a classe "active" ao nav-item clicado
-    $(this).addClass('active');
-
-    // Atualizar o slide do carrossel com base no índice
-    $('#carouselExampleIndicators').carousel(index);
+    $('#carouselExampleIndicators').on('slid.bs.carousel', function() {
+      // Obtém o data-referencia do slide ativo
+      var activeSlideReferencia = $(this).find('.carousel-item.active').data('referencia');
+  
+      // Remover a classe active dos <li>
+      $('.navbar-nav .nav-item').removeClass('active');
+      var elementoID = 'nav-' + activeSlideReferencia;
+      //console.log(elementoID);
+      var elemento = document.getElementById(elementoID);
+      //console.log(elemento);
+      elemento.classList.add('active');
+    });
   });
-
-  // Manipular o evento de slide do carrossel
-  $('#carouselExampleIndicators').on('slide.bs.carousel', function (event) {
-    // Obter o índice do slide ativo
-    var index = event.to;
-
-    // Remover todos os "active" dos nav-items
-    $('.nav-item').removeClass('active');
-    
-    // Adicionar a classe "active" ao nav-item correspondente ao slide
-    $('.nav-item').eq(index).addClass('active');
-  });
-});
   
